dbe25ff19e7319c32f84130a7d7a6c374e257fe5
Kirill Runets <rkmwork97@gmail.com>
2017-11-09 16:54:53 +0300

    The Id has been added to triangle

--- src/com/runets/trainingproject/entity/Triangle.java
+++ src/com/runets/trainingproject/entity/Triangle.java
@@ -5,9 +5,17 @@ import org.apache.logging.log4j.Logger;
 
 public class Triangle {
     private static Logger Logger = LogManager.getLogger(Triangle.class);
+    private int id;
     private Point a;
     private Point b;
     private Point c;
+
+    public Triangle(int id, Point a, Point b, Point c) {
+        this.id = id;
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
 
     public Triangle(Point a, Point b, Point c) {
         this.a = a;
@@ -21,6 +29,12 @@ public class Triangle {
         c = new Point(xC, yC);
     }
 
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
     public Point getA() {
         return a;
     }
@@ -41,27 +55,32 @@ public class Triangle {
     }
 
     @Override
-    public int hashCode() {
-        return (31 * (a != null ? a.hashCode() : 0) + (b != null ?
-                b.hashCode() : 0) + (c != null ? c.hashCode() : 0));
-    }
-
-    @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
 
         Triangle triangle = (Triangle) o;
 
+        if (id != triangle.id) return false;
         if (a != null ? !a.equals(triangle.a) : triangle.a != null) return false;
         if (b != null ? !b.equals(triangle.b) : triangle.b != null) return false;
         return c != null ? c.equals(triangle.c) : triangle.c == null;
     }
 
     @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (a != null ? a.hashCode() : 0);
+        result = 31 * result + (b != null ? b.hashCode() : 0);
+        result = 31 * result + (c != null ? c.hashCode() : 0);
+        return result;
+    }
+
+    @Override
     public String toString() {
         return "Triangle{" +
-                "a=" + a +
+                "id=" + id +
+                ", a=" + a +
                 ", b=" + b +
                 ", c=" + c +
                 '}';
